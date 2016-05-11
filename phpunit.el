c336bd4aed6789750472392d97ba56df4b43062a
Eric Hansen <echansen@nabancard.com>
2016-05-11 13:10:58 -0400

    There is a new test function to get the test

--- phpunit.el
+++ phpunit.el
@@ -32,7 +32,7 @@
 
 ;; Thanks to tox.el(https://github.com/chmouel/tox.el) from Chmouel Boudjnah.
 
-;; To use this code, bind the functions `phpunit-current-test', `phpunit-current-class'
+;; To use this code, bind the functions `phpunit-current-test', `phpunit-current-class', `php-helm-select-test`
 ;; and `phpunit-current-project' to convenient keys with something like :
 
 ;; (define-key web-mode-map (kbd "C-x t") 'phpunit-current-test)
