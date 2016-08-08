109145660dd77d86ee37d6635641f7d324388a78
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2016-08-08 10:08:45 +0200

    Update copyright
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- phpunit-mode.el
+++ phpunit-mode.el
@@ -1,5 +1,7 @@
 ;;; phpunit-mode.el --- Minor mode for PHPUnit
 
+;; Copyright (C) 2014-2016 Nicolas Lamirault <nicolas.lamirault@gmail.com>
+
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License
 ;; as published by the Free Software Foundation; either version 2
