1f9166df641011f3bab6d9ef5eec830d67692818
USAMI Kenta <tadsan@zonu.me>
2016-09-14 21:03:07 +0900

    Modify phpunit detection algorithm

--- phpunit.el
+++ phpunit.el
@@ -133,20 +133,21 @@
         (filename (or (buffer-file-name) ""))
         (vendor-dir (locate-dominating-file "" "vendor")))
     (setq phpunit-executable
-          (if (and vendor-dir (file-exists-p (concat vendor-dir "vendor/bin/phpunit")))
-              (concat vendor-dir "vendor/bin/phpunit")
-            (executable-find "phpunit")))
-    ;; (setq phpunit-executable
-    ;;       (concat (locate-dominating-file filename "vendor")
-    ;;               "vendor/bin/phpunit"))
+          (cond ((stringp phpunit-program) phpunit-program)
+                ((functionp phpunit-program) (funcall phpunit-program))
+                ((and vendor-dir (file-exists-p (concat vendor-dir "vendor/bin/phpunit")))
+                 (concat vendor-dir "vendor/bin/phpunit"))))
     (unless phpunit-executable
-      (setq phpunit-executable phpunit-program))
+      (setq phpunit-executable "phpunit"))
     (when (file-remote-p phpunit-executable)
       (setq phpunit-executable
             (tramp-file-name-localname (tramp-dissect-file-name phpunit-executable))))
     (s-concat phpunit-executable
+              (when phpunit-arg
+                (s-concat " " (if (stringp phpunit-arg) phpunit-arg
+                                (s-join " " (mapcar 'shell-quote-argument phpunit-arg)))))
               (if phpunit-configuration-file
-                  (s-concat " -c " phpunit-configuration-file)
+                  (s-concat " -c " (shell-quote-argument phpunit-configuration-file))
                 "")
               " "
               args)))
